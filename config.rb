9084cc4e112d840db35b5fae78c58f8043f6e397
nixi-awoo <nixi@localhost.localdomain>
2015-09-04 20:11:13 +0200

    Added the landing page template.

--- config.rb
+++ config.rb
@@ -6,8 +6,8 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
-images_dir = "imag"
+sass_dir = "scss"
+images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
